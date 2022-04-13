baa436646ecbb24df2f714cc9f986dd5ad0154ef
Alexey Lysenko <abesmon@gmail.com>
2022-04-13 18:04:17 +0300

    update to use with modern tools

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version:5.3
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyCam"
+    name: "SwiftyCam",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_14)
+    ],
+    products: [
+        .library(name: "SwiftyCam", targets: ["SwiftyCam"])
+    ],
+    targets: [
+        .target(name: "SwiftyCam", path: "Source")
+    ]
 )
